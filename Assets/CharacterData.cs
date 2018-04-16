b29d03b9d9c994f8857049a9ae15ae1307c9e87b
Unknown <transkuja@hotmail.fr>
2018-04-16 14:45:44 +0200

    Start working on character data and game initialization

---
+++ Assets/CharacterData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData : MonoBehaviour {
+
+    [SerializeField]
+    int health;
+    [SerializeField]
+    int owner;
+
+    public bool hasControl;
+    public GameObject cameraRef;
+
+    bool isCharacterInitialized = false;
+
+    public void Init(int _health, int _owner, bool _hasControl)
+    {
+        health = _health;
+        owner = _owner;
+        hasControl = _hasControl;
+        isCharacterInitialized = true;
+        cameraRef = GetComponentInChildren<Cinemachine.CinemachineVirtualCamera>().gameObject;
+        cameraRef.SetActive(false);
+    }
+
+}
---
+++ Assets/TurnHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnHandler : MonoBehaviour {
+
+    [SerializeField]
+    GameObject character;
+
+    [SerializeField]
+    Transform startingPositions;
+
+    List<CharacterData>[] characters;
+
+    public int numberOfPlayers = 2;
+
+	void Start () {
+        characters = new List<CharacterData>[numberOfPlayers];
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            characters[i] = new List<CharacterData>();
+        }
+
+        for (int i = 0; i < startingPositions.childCount; i++)
+        {
+            GameObject newCharacter = Instantiate(character, startingPositions.GetChild(i).position, startingPositions.GetChild(i).rotation, null);
+            newCharacter.GetComponent<CharacterData>().Init(100, i % numberOfPlayers, false);
+            characters[i % numberOfPlayers].Add(newCharacter.GetComponent<CharacterData>());
+        }
+
+        int startingPlayer = Random.Range(0, numberOfPlayers);
+        characters[startingPlayer][0].cameraRef.SetActive(true);
+        characters[startingPlayer][0].hasControl = true;
+
+    }
+	
+	void Update () {
+		
+	}
+}
