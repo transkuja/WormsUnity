2af1fdf7bc9724b0b8a47071f94bf297776b4ff8
Unknown <transkuja@hotmail.fr>
2018-04-17 09:07:41 +0200

    Handle worldspace canvas orientation

---
+++ Assets/BillboardCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardCamera : MonoBehaviour {
+	
+	void Update () {
+        Vector3 rotateToward = Camera.main.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(-rotateToward, Vector3.up);
+	}
+}
