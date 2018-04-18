7b60f7d557198346974124ac0aa55c3c4f3c237a
Unknown <transkuja@hotmail.fr>
2018-04-18 22:19:45 +0200

    Add replay

---
+++ Assets/ReplayScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayScreen : MonoBehaviour {
+    
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
