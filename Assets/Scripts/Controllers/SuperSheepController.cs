c8a932c5283bbcb4cb3bfce5ff9799381a17f42a
Unknown <transkuja@hotmail.fr>
2018-04-19 15:11:27 +0200

    Start working on Super Sheep (need controls + camera)

---
+++ Assets/Scripts/Controllers/SuperSheepController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperSheepController : MonoBehaviour {
+    Rigidbody rb;
+    public float sheepSpeed;
+    public Rigidbody Rb
+    {
+        get
+        {
+            if (rb == null)
+                rb = GetComponentInChildren<Rigidbody>();
+            return rb;
+        }
+
+        set
+        {
+            rb = value;
+        }
+    }
+
+    void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody>();
+        rb.useGravity = false;
+        Rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        Rb.AddForce((transform.forward + Vector3.up) * 2.0f, ForceMode.Impulse);
+        Rb.AddForce(transform.forward * sheepSpeed, ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+        Rb.AddForce(transform.forward * sheepSpeed);
+    }
+}
